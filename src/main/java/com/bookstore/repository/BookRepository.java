39f933920b36fc5418bd93e653d8fb4d67e389e0
Deepa Kumari <deepafeb01@gmail.com>
2026-04-25 17:48:36 +0530

    Initial commit - Book Store Management System

---
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.bookstore.entity.Book;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Integer>{
+	List<Book> findByNameContainingIgnoreCase(String keyword);
+}
